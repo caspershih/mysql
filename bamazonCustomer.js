bde9f34d58a9392b5b21f966007d2854c100903b
Casper Shih <casshih@gmail.com>
2018-09-23 10:45:45 -0700

    improving

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,1 +1,30 @@
-var 
+// Dependencies
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var sqlConnection = mysql.createConnection( {
+    host: "localhost",
+    port: 3306,
+
+// Database credential
+    user: "root",
+    password: "root",
+    database: "bamzon"
+});
+
+connection.connect(function(error) {
+    if (error) {
+        console.log(error);
+    }
+    loadProducts();
+});
+
+function loadProducts() {
+
+    console.log("SELECT * FROM products", function(error, res) {
+        if (error) throw error;
+
+        amazonItems(res);
+    });
+}
+
